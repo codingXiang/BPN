4bed3fe901c4046e889ef668d45781f4f582e014
CodingXiang <codingxiang@smail.nchu.edu.tw>
2017-10-19 15:56:47 +0800

    修改迭代終止條件

--- bpnn.py
+++ bpnn.py
@@ -6,9 +6,10 @@ from diagram import Diagram
 class BPNN(object):
     def __init__(self , dataset , learning_rate = 0.01 , n_iter = 10000 , momentum = 0.9 , shutdown_condition = 0.01):
         self.n_iter = n_iter
+        self.dataset = dataset
         self.learning_rate = learning_rate
-        self.x = dataset.training_data
-        self.Y = dataset.output
+        self.x = dataset.train_x
+        self.Y = dataset.train_Y
         self.shutdown_condition = shutdown_condition
         self.cost = []
         self.momentum = momentum
@@ -24,16 +25,24 @@ class BPNN(object):
         self.hidden_node = int((self.input_node + self.output_node) / 2)
 
         # bias
-        self.h_b = np.random.random(self.hidden_node)
-        self.y_b = np.random.random(self.output_node)
+        self.h_b = np.random.random(self.hidden_node) * 0.3 + 0.1
+        self.y_b = np.random.random(self.output_node) * 0.3 + 0.1
     # step2
     def set_weight(self):
         self.w1 = np.random.random((self.input_node , self.hidden_node))
         self.w2 = np.random.random((self.hidden_node , self.output_node))
     # step3
-    def forward(self):
-        self.h = self.sigmoid((np.dot(self.x , self.w1) + self.h_b))
+    def predict(self , x , Y):
+        self.h = self.sigmoid((np.dot(x , self.w1) + self.h_b))
         self.y = self.sigmoid((np.dot(self.h , self.w2) + self.y_b))
+        zy = np.where(self.y > 0.5 , 1 , 0)
+        p_y = Y - zy
+        self.acc = 0
+        for i in p_y:
+            if (i.sum() == 0):
+                self.acc += 1
+        self.acc = self.acc / Y.shape[0] * 100.0
+        return self
     # step4
     def backend(self):
         E = (self.Y - self.y)
@@ -43,28 +52,30 @@ class BPNN(object):
         ### 隱藏層 delta 計算
         delta_h = (1 - self.h) * self.h * np.dot(delta_y , self.w2.T)
         self.w2 += self.learning_rate * self.h.T.dot(delta_y) + self.momentum * self.h.T.dot(delta_y)
-        self.w1 += self.learning_rate * self.x.T.dot(delta_h) + self.momentum * self.x.T.dot(delta_h) 
+        self.w1 += self.learning_rate * self.x.T.dot(delta_h) + self.momentum * self.x.T.dot(delta_h)
+#         self.w2 += self.learning_rate * self.h.T.dot(delta_y)
+#         self.w1 += self.learning_rate * self.x.T.dot(delta_h)
+
         return errors
 
     def train(self):
         self.error = 0
         for _iter in range(0 , self.n_iter):
-            self.forward()
+            self.predict(self.x , self.Y)
             self.error = self.backend()
             self.cost.append(self.error)
-            if (self.error <= self.shutdown_condition):
+            if (_iter % 1000 == 0):
+                print("正確率為：%.2f" % self.acc)
+#             if (self.error <= self.shutdown_condition):
+#                 return self
+            if (self.acc == 100.0):
                 return self
         return self
+
     def test(self):
-        self.forward()
-        zy = np.where(self.y > 0.5 , 1 , 0)
-        p_y = self.Y - zy
-        self.acc = 0
-        for i in p_y:
-            if (i.sum() == 0):
-                self.acc += 1
-        self.acc = self.acc / self.x.shape[0] * 100
+        self.predict(self.dataset.test_x , self.dataset.test_Y)
         return self
+
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
     def draw(self , xlabel = '' , ylabel = '' , legend_loc = '' , title = ''):
